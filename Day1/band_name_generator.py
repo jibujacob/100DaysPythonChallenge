8ae85891210894d52949681fcac16cf38caf83f7
jibujacob <jibupjacob@gmail.com>
2021-11-22 12:57:03 +0530

    Day 1 Task Completed

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,5 @@
+#Band Name Generator Task
+print("Welcome to Band Name Generator.")
+city_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's your pet's name?Ban\n")
+print("Your band name could be " + city_name+" "+pet_name)
